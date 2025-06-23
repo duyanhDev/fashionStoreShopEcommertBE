430ebbc26a78ad98483161f46f7c460c7188a810
DuyanhDev <166149554+duyanhDev@users.noreply.github.com>
2025-06-23 17:15:06 +0700

    deloy be ban 2

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -24,7 +24,9 @@ router.get(
     });
 
     // Chuyển hướng về frontend với token
-    res.redirect(`http://localhost:5173/auth/callback?token=${token}`);
+    res.redirect(
+      `https://fashion-store-shop-ecommert.vercel.app/?token=${token}`
+    );
   }
 );
 
